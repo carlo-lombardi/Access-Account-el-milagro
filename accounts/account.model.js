913ccaf25c98d3905bf42b7eaeef093a5a1f93b7
Carlo Lombardi <carlo_lombardi@users.noreply.github.com>
2021-08-31 10:17:54 +0100

    main push of authentication

--- accounts/account.model.js
+++ accounts/account.model.js
@@ -1,37 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const schema = new Schema({
-    email: { type: String, unique: true, required: true },
-    passwordHash: { type: String, required: true },
-    title: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    acceptTerms: Boolean,
-    role: { type: String, required: true },
-    verificationToken: String,
-    verified: Date,
-    resetToken: {
-        token: String,
-        expires: Date
-    },
-    passwordReset: Date,
-    created: { type: Date, default: Date.now },
-    updated: Date
+const userSchema = new Schema({
+  email: { type: String, unique: true, required: true },
+  passwordHash: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  phone: { type: String, required: false },
+  order: { type: Object, required: false },
+  isRegistered: { type: Boolean, required: false },
+  acceptTerms: { type: Boolean, required: false },
+  role: { type: String, required: true },
+  verificationToken: String,
+  verified: Date,
+  resetToken: {
+    token: String,
+    expires: Date,
+  },
+  passwordReset: Date,
+  created: { type: Date, default: Date.now },
+  updated: Date,
 });
 
-schema.virtual('isVerified').get(function () {
-    return !!(this.verified || this.passwordReset);
+userSchema.virtual("isVerified").get(function () {
+  return !!(this.verified || this.passwordReset);
 });
 
-schema.set('toJSON', {
-    virtuals: true,
-    versionKey: false,
-    transform: function (doc, ret) {
-        // remove these props when object is serialized
-        delete ret._id;
-        delete ret.passwordHash;
-    }
+userSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    // remove these props when object is serialized
+    delete ret._id;
+    delete ret.passwordHash;
+  },
 });
 
-module.exports = mongoose.model('Account', schema);
+module.exports = mongoose.model("users", userSchema);
--- server.js
+++ server.js
@@ -1,29 +1,57 @@
-﻿require('rootpath')();
-const express = require('express');
+﻿require("rootpath")();
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const errorHandler = require('_middleware/error-handler');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const errorHandler = require("_middleware/error-handler");
+const nodemailer = require("nodemailer");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 // allow cors requests from any origin and with credentials
-app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+app.use(
+  cors({
+    origin: (origin, callback) => callback(null, true),
+    credentials: true,
+  })
+);
 
 // api routes
-app.use('/accounts', require('./accounts/accounts.controller'));
+app.use("/accounts", require("./accounts/accounts.controller"));
 
 // swagger docs route
-app.use('/api-docs', require('_helpers/swagger'));
+app.use("/api-docs", require("_helpers/swagger"));
 
 // global error handler
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  auth: {
+    user: "shany.franecki51@ethereal.email",
+    pass: "kC6CDBjC6yHeD4gjAZ",
+  },
+});
+const mailDetails = {
+  from: "shany.franecki51@ethereal.email",
+  to: "mr.lombardi@outlook.com",
+  subject: "Test mail",
+  text: "Soy un conchadesumadre",
+};
+transporter.sendMail(mailDetails, function (err, data) {
+  if (err) {
+    console.log("Error Occurs");
+  } else {
+    console.log("Email sent successfully");
+  }
+});
+const port =
+  process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000;
 app.listen(port, () => {
-    console.log('Server listening on port ' + port);
+  console.log("Server listening on port " + port);
 });
